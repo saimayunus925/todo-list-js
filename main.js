7d5eb4bff59c2c259712939442236218b76f0460
Saima Yunus <saimayunus925@gmail.com>
2023-01-22 23:44:22 -0800

    added basic 'addItem' form

---
+++ main.js
@@ -0,0 +1,7 @@
+
+// objective: create a functioning CRUD to-do list app with just HTML and JavaScript (and later CSS, perhaps)
+
+var todoListJS = document.getElementById("todoList"); // getting our list from the HTML file using its ID
+
+var addItemForm = document.getElementById("addItem"); // form for adding items to to-do list
+
